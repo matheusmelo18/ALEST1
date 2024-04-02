903b7ffaec695d0fc1b7b11d28fc9f807854eb98
Matheus Melo <matheusmelo180203@gmail.com>
2024-04-01 21:22:51 -0300

    falta a parte de escrever/format para .txt

--- Main.java
+++ Main.java
@@ -1,152 +1,43 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
-
-public class Main{
-  
-
+public class Main {
 
     public static void main(String[] args) {
-        String arquivoCSV = "transacoes_00020.csv";
-        String linha;
-        String[] dados;
-        int[][] vendasPorLojaMes = new int[4][12];
-        int totalGeralVendas = 0;
-        
-        Leitura leitura = new Leitura();
-        try(BufferedReader br = new BufferedReader(new FileReader(arquivoCSV))){
-            br.readLine();
-            while((linha = br.readLine()) != null){
-                dados = linha.split(",");
-                int loja = Integer.parseInt(dados[2]);
-                int mes = Integer.parseInt(dados[1]);
-                int valor = Integer.parseInt(dados[3]);
-                vendasPorLojaMes[loja-1][mes-1] += valor;
-                totalGeralVendas += valor;
+        Path arquivoEntrada = Paths.get("./ALEST1/CSV/transacoes_00500.csv");
+        Path arquivoSaida = Paths.get("resultados.txt");
 
-                int indiceLoja = obterIndiceLoja(String.valueOf(loja));
-                int indiceMes = obterIndiceMes(String.valueOf(mes));
-                vendasPorLojaMes[indiceLoja][indiceMes] += valor;
+        try (BufferedReader br = Files.newBufferedReader(arquivoEntrada);
+                PrintWriter pd = new PrintWriter(new FileWriter(arquivoSaida.toFile()))) {
 
-                totalGeralVendas += valor;
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }   
-        int menorVenda = Integer.MAX_VALUE;
-        int indiceMenorVenda=-1;
-        int[] maiorVendas = new int[3];
-        String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
-
-        try{
-            FileWriter write = new FileWriter("resultados.txt");
-            
-            write.write("Matriz de vendas");
-            write.write(" ");
-
-            for(String mes : meses){
-                write.write(mes + "  ");
-            }
-            write.write("\n");
-            String[] lojas = {"Matriz", "Filial Sul", "Filial Norte", "Filial Nordeste"};
-            for(int i=0; i<4; i++){
-                write.write(lojas[i] + " ");
-                for(int j=0; j<12; j++){
-                    write.write(vendasPorLojaMes[i][j] + " ");
-                }
-                write.write("\n");
+            Map<String, Integer> transacoes = new HashMap<>();
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                String[] dados = linha.split(",");
+                String produto = dados[0];
+                int quantidade = Integer.parseInt(dados[1]);
+                transacoes.put(produto, transacoes.getOrDefault(produto, 0) + quantidade);
             }
 
-            write.write("\n tres meses com mais vendas");
-            for(int i=0; i<3; i++){
-                int maiorVenda = Integer.MIN_VALUE;
-                for(int j=0; j<4; j++){
-                    for(int k=0; k<12; k++){
-                        if(vendasPorLojaMes[j][k] > maiorVenda){
-                            maiorVenda = vendasPorLojaMes[j][k];
-                            maiorVendas[i] = maiorVenda;
-                        }
-                    }
-                }
-                write.write(maiorVenda + " ");
+            // Adding a header to the output file
+            pd.println("Product: Quantity");
+
+            for (Map.Entry<String, Integer> entry : transacoes.entrySet()) {
+                String produto = entry.getKey();
+                int quantidade = entry.getValue();
+                // Formatting the output with a specific separator
+                pd.println(produto + " - " + quantidade);
             }
 
-            write.write("\nmenor venda");
-            for(int i=0; i < 12; i++){
-                for(int j=0; j<4; j++){
-                    if(vendasPorLojaMes[j][i] < menorVenda){
-                        menorVenda = vendasPorLojaMes[j][i];
-                        indiceMenorVenda = i;
-                    }
-                }
-            }
-            write.write(meses[indiceMenorVenda] + " " + menorVenda);
-
-
-            write.write("total geral de vendas");
-            write.write(String.valueOf(totalGeralVendas));
-
-
-            write.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    
-    
-    
-    }
-
-    private static int obterIndiceLoja(String loja) {
-        switch (loja) {
-            case "Matriz":
-                return 0;
-            case "Filial Sul":
-                return 1;
-            case "Filial Norte":
-                return 2;
-            case "Filial Nordeste":
-                return 3;
-            default:
-                return -1;
+        } catch (IOException e) {
+            System.out.println("Erro ao processar o arquivo: " + e.getMessage());
         }
     }
-
-
-
-        private static int obterIndiceMes(String mes) {
-            switch (mes) {
-                case "Janeiro":
-                    return 1;
-                case "Fevereiro":
-                    return 2;
-                case "Março":
-                    return 3;
-                case "Abril":
-                    return 4;
-                case "Maio":
-                    return 5;
-                case "Junho":
-                    return 6;
-                case "Julho":
-                    return 7;
-                case "Agosto":
-                    return 8;
-                case "Setembro":
-                    return 9;
-                case "Outubro":
-                    return 10;
-                case "Novembro":
-                    return 11;
-                case "Dezembro":
-                    return 12;
-                default:
-                    return -1;
-            }
-        }
-    } 
-
+}
